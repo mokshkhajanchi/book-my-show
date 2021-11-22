7fb78bbe28dff881f46c5c659d0e3f464c23ab6c
moksh <mokshkhajanchi95@gmail.com>
2021-11-22 18:09:52 +0530

    Settings changed

--- bms/settings.py
+++ bms/settings.py
@@ -218,6 +218,9 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
